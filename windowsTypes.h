72096deb8f6f17dde6d4e92e62f83ce2f2c2fea6
Matt Kucenski <matthew.kucenski@doe.gov>
2017-03-21 11:26:46 -0600

    simplying the way file attribute flags are matched and cleanup on SID structures

--- windowsTypes.h
+++ windowsTypes.h
@@ -15,6 +15,8 @@
 #ifndef _WINDOWSTYPES_H_
 #define _WINDOWSTYPES_H_
 
+#include "misc/coded-message.h"
+
 #include <sys/types.h>
 
 typedef u_int8_t  BYTE;
@@ -31,32 +33,41 @@ typedef struct _FILETIME {
 } __attribute__((packed)) FILETIME;
 
 // FILE ATTRIBUTE CONSTANTS (https://msdn.microsoft.com/en-us/library/gg258117(v=vs.85).aspx)
-#define FILE_ATTRIBUTE_ARCHIVE					0x20
-#define FILE_ATTRIBUTE_COMPRESSED				0x800
-#define FILE_ATTRIBUTE_DEVICE						0x40
-#define FILE_ATTRIBUTE_DIRECTORY					0x10
-#define FILE_ATTRIBUTE_ENCRYPTED					0x4000
-#define FILE_ATTRIBUTE_HIDDEN						0x2
-#define FILE_ATTRIBUTE_INTEGRITY_STREAM		0x8000
-#define FILE_ATTRIBUTE_NORMAL						0x80
-#define FILE_ATTRIBUTE_NOT_CONTENT_INDEXED	0x2000
-#define FILE_ATTRIBUTE_NO_SCRUB_DATA			0x20000
-#define FILE_ATTRIBUTE_OFFLINE					0x1000
-#define FILE_ATTRIBUTE_READONLY					0x1
-#define FILE_ATTRIBUTE_REPARSE_POINT			0x400
-#define FILE_ATTRIBUTE_SPARSE_FILE				0x200
-#define FILE_ATTRIBUTE_SYSTEM						0x4
-#define FILE_ATTRIBUTE_TEMPORARY					0x100
-#define FILE_ATTRIBUTE_VIRTUAL					0x10000
+static coded_message_t FILE_ATTRIBUTES[] = {
+	{"FILE_ATTRIBUTE_ARCHIVE",					0x20,		""},
+	{"FILE_ATTRIBUTE_COMPRESSED",				0x800,	""},
+	{"FILE_ATTRIBUTE_DEVICE",					0x40,		""},
+	{"FILE_ATTRIBUTE_DIRECTORY",				0x10,		""},
+	{"FILE_ATTRIBUTE_ENCRYPTED",				0x4000,	""},
+	{"FILE_ATTRIBUTE_HIDDEN",					0x2,		""},
+	{"FILE_ATTRIBUTE_INTEGRITY_STREAM",		0x8000,	""},
+	{"FILE_ATTRIBUTE_NORMAL",					0x80,		""},
+	{"FILE_ATTRIBUTE_NOT_CONTENT_INDEXED",	0x2000,	""},
+	{"FILE_ATTRIBUTE_NO_SCRUB_DATA",			0x20000,	""},
+	{"FILE_ATTRIBUTE_OFFLINE",					0x1000,	""},
+	{"FILE_ATTRIBUTE_READONLY",				0x1,		""},
+	{"FILE_ATTRIBUTE_REPARSE_POINT",			0x400,	""},
+	{"FILE_ATTRIBUTE_SPARSE_FILE",			0x200,	""},
+	{"FILE_ATTRIBUTE_SYSTEM",					0x4,		""},
+	{"FILE_ATTRIBUTE_TEMPORARY",				0x100,	""},
+	{"FILE_ATTRIBUTE_VIRTUAL",,				0x10000,	""}
+};
 
 // SID STRUCTURE (https://blogs.msdn.microsoft.com/larryosterman/2004/09/01/what-is-this-thing-called-sid/)
 // Conversion to String: "S-<bRevision>-<bIdentifierAuthority>-<dwSubAuthority#>..."
+typedef struct _SID_IDENTIFIER_AUTHORITY {
+    BYTE  bValue[6];
+} __attribute__((packed)) SID_IDENTIFIER_AUTHORITY;
+#define SID_IDENTIFIER_AUTHORITY_LENGTH 6
+#define SID_MAX_SUB_AUTHORITIES 15
+
 typedef struct _SID {
-	BYTE bRevision;
-	BYTE bSubAuthorityCount;
-	BYTE bIdentifierAuthority[6];
-	//DWORD dwSubAuthority[bSubAuthorityCount];
+   BYTE  bRevision;
+   BYTE  bSubAuthorityCount;
+   SID_IDENTIFIER_AUTHORITY identifierAuthority;
+   DWORD dwSubAuthority[SID_MAX_SUB_AUTHORITIES];
 } __attribute__((packed)) SID;
+#define SID_LENGTH 68
 
 #define SECURITY_NULL_SID_AUTHORITY         {0,0,0,0,0,0}
 #define SECURITY_WORLD_SID_AUTHORITY        {0,0,0,0,0,1}
