208b847c553e67b9903e297f9195e2bf729b594f
Matt Kucenski <matthew.kucenski@doe.gov>
2017-03-28 11:57:35 -0600

    adding hex dump debug function

--- debugMsgs.h
+++ debugMsgs.h
@@ -16,14 +16,20 @@
 #define _DEBUGMSGS_H_
 
 #ifdef _DEBUG_
+
 #include <iostream>
-#define DEBUG_ERROR(msg)	(std::cerr << "DEBUG ERROR: " << msg << std::endl)
-#define DEBUG_WARNING(msg)	(std::cerr << "DEBUG WARNING: " << msg << std::endl)
-#define DEBUG_INFO(msg)		(std::cerr << "DEBUG INFO: " << msg << std::endl)
+#define DEBUG_ERROR(msg)								  (std::cerr << "DEBUG ERROR:   " << msg << std::endl)
+#define DEBUG_WARNING(msg)								  (std::cerr << "DEBUG WARNING: " << msg << std::endl)
+#define DEBUG_INFO(msg)									  (std::cerr << "DEBUG INFO:    " << msg << std::endl)
+#define DEBUG_HEX_DUMP(p_rgData, count, width)	fprintf(stderr, "DEBUG HEX:     "); u_int8_t* p_rgCastData = (u_int8_t*)p_rgData; for (int i=0; i<count; i++) { fprintf(stderr, "%s%02x", (i % width == 0 ? "\n\t" : " "), p_rgCastData[i]); } fprintf(stderr, "\n");
+
 #else
+
 #define DEBUG_ERROR(msg)
 #define DEBUG_WARNING(msg)
 #define DEBUG_INFO(msg)
+#define DEBUG_HEX_DUMP(p_rgData, count, width)
+
 #endif
 
 #endif //_DEBUGMSGS_H_
