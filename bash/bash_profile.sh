7449f16cb869de1283c3df83e6cbb194bf9c4f53
Matt Kucenski <mkucenski@users.noreply.github.com>
2021-04-12 22:17:31 -0700

    cleaning up bash shell environment scripts

--- bash/bash_profile.sh
+++ bash/bash_profile.sh
@@ -1,7 +1,20 @@
-# .bash_profile is executed for login shells
-# Except macOS -- all Terminal shells are "login"
+# .bash_profile (this file) is executed for "login"
+# shells; except macOS -- all macOS Terminal shells
+# are "login"
+#
+# Sourcing .bashrc to make it the definitive
+# location for all bash shell environments
 
 if [ -f "$HOME/.bashrc" ]; then
 	source "$HOME/.bashrc"
 fi
+
+
+##
+# Your previous /Users/mkucenski/.bash_profile file was backed up as /Users/mkucenski/.bash_profile.macports-saved_2021-04-12_at_21:38:52
+##
+
+# MacPorts Installer addition on 2021-04-12_at_21:38:52: adding an appropriate DISPLAY variable for use with MacPorts.
+export DISPLAY=:0
+# Finished adapting your DISPLAY environment variable for use with MacPorts.
 
--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -1,42 +1,31 @@
 # .bashrc is executed for interactive, but non-login shells (i.e. subsequent shells after the first login)
 
+UNAME="$(uname)"
+
+# ENVIRONMENT VARIABLES
+
 HISTFILESIZE=400000000
 HISTSIZE=10000
 HISTTIMEFORMAT="%Y%m%d %H:%M:%S "
 PROMPT_COMMAND="history -a"
-
 shopt -s histappend
 
-export PATH="$HOME/Scripts:$HOME/Scripts/macOS:$PATH"
-export PATH="$HOME/Scripts:$HOME/Scripts/tsk:$PATH"
+export PATH="$HOME/Scripts:$PATH"
+export PATH="$HOME/Scripts/tsk:$PATH"
 export PATH="$HOME/Development/opt/bin:$HOME/Development/opt/sbin:$PATH"
-
 export MANPATH="$HOME/Development/opt/share/man:$MANPATH"
 
-UNAME="$(uname)"
+export PATH="$HOME/perl5/bin${PATH:+:${PATH}}"
+export PERL5LIB="$HOME/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"
+export PERL_LOCAL_LIB_ROOT="$HOME/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"
+export PERL_MB_OPT="--install_base \"$HOME/perl5\""
+export PERL_MM_OPT="INSTALL_BASE=$HOME/perl5"
 
-if [ "$UNAME" == "FreeBSD" ]; then
-
-	EDITOR=vi;   	export EDITOR
-	PAGER=more;  	export PAGER
-
-	# set ENV to a file invoked each time sh is started for interactive use.
-	ENV=$HOME/.shrc; export ENV
-
-	# if [ -x /usr/games/fortune ] ; then /usr/games/fortune freebsd-tips ; fi
-
-	eval $(ssh-agent -s) > /dev/null
-
-elif [ "$UNAME" == "Linux" ]; then
-
-	eval $(ssh-agent -s) > /dev/null
-
-elif [ "$UNAME" == "Darwin" ]; then
+if [ "$UNAME" == "Darwin" ]; then
 	export PATH="$HOME/Scripts/macOS:$PATH"
 	export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
 	export PATH="/Applications/VMware OVF Tool:$PATH"
 	export PATH="$HOME/Python/bin:$PATH"
-
 	export MANPATH="/opt/local/share/man:$MANPATH"
 
 	# Python...
@@ -45,30 +34,38 @@ elif [ "$UNAME" == "Darwin" ]; then
 
 	# Java
 	export JAVA_HOME=$(/usr/libexec/java_home)
-	alias bfg="java -jar $HOME/Development/GitHub/rtyley/bfg-repo-cleaner/bfg/target/bfg-1.13.1-SNAPSHOT-master-aeee9e3.jar"
 else
-	echo "Unknown Operating System!"
+	if [ "$UNAME" == "FreeBSD" ]; then
+		EDITOR=vi;   	export EDITOR
+		PAGER=less;  	export PAGER
+
+		# set ENV to a file invoked each time sh is started for interactive use.
+		ENV=$HOME/.shrc; export ENV
+	fi
 fi
-
-# Add .ssh/id_rsa[.private]
-ssh-add
-
-echo; fortune -a "$HOME/Development/MyGitHub/misc/fortunes/"; echo
 
 alias ll="ls -l"
 alias llh="ls -lh"
 alias cll="clear; ls -l"
 alias cllh="clear; ls -lh"
 alias rot13="tr 'A-Za-z' 'N-ZA-Mn-za-m'"
+#alias info='pinfo'
+#alias man='pinfo'
 
-PATH="$HOME/perl5/bin${PATH:+:${PATH}}"; export PATH;
-PERL5LIB="$HOME/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
-PERL_LOCAL_LIB_ROOT="$HOME/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
-PERL_MB_OPT="--install_base \"$HOME/perl5\""; export PERL_MB_OPT;
-PERL_MM_OPT="INSTALL_BASE=$HOME/perl5"; export PERL_MM_OPT;
+# LOCAL-HOST SPECIFIC
 
-if [ -f "$HOME/.bashrc" ]; then
+if [ -f "$HOME/.bashrc_local" ]; then
 	source "$HOME/.bashrc_local"
 fi
 
+# EXECUTABLES
 
+if [ "$UNAME" != "Darwin" ]; then
+	eval $(ssh-agent -s) > /dev/null
+	ssh-add
+	ssh-add "$HOME/.ssh/mygithub_rsa_personal"
+fi
+
+echo; uname -a
+if [ $(which cpufetch) ]; then echo; cpufetch 2>/dev/null | tail -n +3; fi
+if [ $(which fortune) ]; then echo; fortune -a "$HOME/Development/MyGitHub/misc/fortunes/"; fi
